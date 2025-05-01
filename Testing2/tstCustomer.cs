f9cc4c1d8c989606e20c5dd043ca7d63d92e40df
p2785371 <p2785371@my365.dmu.ac.uk>
2025-05-01 11:50:09 +0100

    added testing for customer class

---
+++ Testing2/tstCustomer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    internal class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instnace of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
