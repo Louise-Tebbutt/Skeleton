d377391dcfef21558b82ca4a91f4b286c7f31e21
Bayonle Aladenika <aladenikabayonle@gmail.com>
2025-05-07 10:57:42 +0100

    Almost done assigning the properties and data types

---
+++ Testing4/tstStock.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+           ClsStock AStock = new ClsStock();
+
+            // test to see that it exists
+            Assert.IsNotNull(AStock);
+        }
+
+        [TestMethod]
+        public void GameIdPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            ClsStock Astock = new ClsStock();
+            //create some test data to assign to the property 
+            int TestData = 1;
+            //assign the data to the property 
+            Astock.GameId = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(Astock.GameId,TestData);
+        }
+        /*
+        [TestMethod]
+         public void GameTitlePropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsStock Astock = new clsStock();
+            //create some test data to assign to the property 
+            string TestData = ''Elden Ring'';
+            //assign the data to the property 
+            Astock.GameTitle = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(Astock.GameTitle, TestData);
+        }
+        */
+
+        [TestMethod]
+        public void GameTitlePropertyOK()
+        {
+            //create an instance of the class we want to create 
+            ClsStock Astock = new ClsStock();
+            //create some test data to assign to the property 
+            Astock.GameTitle = "Elden Ring";
+            //
+
+        }
+
+
+
+
+    }
+}
