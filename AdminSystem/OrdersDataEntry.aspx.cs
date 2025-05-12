f0cf4cbd7be5be1c5db159bc8aa814ac75ead94f
Acegoanpopout <aladenikabola@gmail.com>
2025-05-12 17:21:01 +0100

    Prac 8!

--- AdminSystem/OrdersDataEntry.aspx.cs
+++ AdminSystem/OrdersDataEntry.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -14,6 +15,22 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
+        //create a new instance of ClsOrder
+        ClsOrder AnOrder =  new ClsOrder();
+        //capture an OrderId 
+        AnOrder.OrderId = Convert.ToInt32(TextBox2.Text);
+        //capture a customerId
+        AnOrder.CustomerId = Convert.ToInt32(TextBox1.Text);
+        //cature the order date
+        AnOrder.OrderDate = Convert.ToDateTime(TextBox3.Text);
+        //capture Total amount
+        AnOrder.TotalAmount = Convert.ToDecimal(TextBox4.Text);
+        //capture Payment Status 
+        AnOrder.PaymentStatus = Convert.ToBoolean(Convert.ToInt32(TextBox5.Text));
+        //capture Shipping Address
+        AnOrder.ShippingAddress = Convert.ToString(TextBox6.Text);
+        //capture StaffId
+        AnOrder.StaffId = Convert.ToInt32(TextBox7.Text);
         //navigate to the view page
         Response.Redirect("OrderDataentry.aspx");
     }
--- AdminSystem/OrdersViewer.aspx.cs
+++ AdminSystem/OrdersViewer.aspx.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance clsOrder
+        ClsOrder AnOrder = new ClsOrder();
+        //get the data from the session object
+        AnOrder = (ClsOrder)Session["AnOrder"];
+        //display if Order is not null
+        if (AnOrder != null)
+        {
+            //display the attribute in clsOrder class
+            Response.Write("OrderId :" + AnOrder.OrderId + "<br />");
+        }
     }
 }
