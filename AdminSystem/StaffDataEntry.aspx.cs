4e41b740d330b0700c8911c4ec79ace666a2d530
P2794998 <lukeha408@gmail.com>
2025-05-17 18:49:43 +0100

    finished prac 9b

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -45,4 +45,36 @@ public partial class _1_DataEntry : System.Web.UI.Page
         // Send user to the view page
         Response.Redirect("StaffViewer.aspx");
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        // Create an instance of the staff class
+        clsStaff AnStaff = new clsStaff();
+
+        // Create a variable to store the primary key
+        Int32 StaffId;
+
+        // Create a variable to store the result of the find operation
+        Boolean found = false;
+
+        // Get the primary key entered by the user
+        StaffId = Convert.ToInt32(txtStaffId.Text);
+
+        // Find the record
+        found = AnStaff.Find(StaffId);
+
+        // If found
+        if (found == true)
+        {
+            // Display the values of the properties in the form
+            txtStaffName.Text = AnStaff.StaffName;
+            txtJoinDate.Text = AnStaff.JoinDate.ToString();
+            txtStaffEmail.Text = AnStaff.StaffEmail;
+            txtStaffPhoneNumber.Text = AnStaff.StaffPhoneNumber;
+            txtStaffSalary.Text = AnStaff.StaffSalary.ToString();
+            chkMorePermissions.Checked = AnStaff.MorePermissions;
+
+
+        }
+    }
 }
