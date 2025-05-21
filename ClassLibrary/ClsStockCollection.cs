dfb34e998f0a5c0ddabf5a400e2036b683e2dae4
Bayonle Aladenika <aladenikabayonle@gmail.com>
2025-05-21 19:32:52 +0100

    Prac 12 half way through

---
+++ ClassLibrary/ClsStockCollection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class ClsStockCollection
+    {
+        //private data member for the list
+        List<ClsStock> mStockList = new List<ClsStock>();
+        //public property for the address list 
+
+        public List<ClsStock> StockList
+        {
+            get
+            {
+                //return the private data 
+                return mStockList;
+            }
+            set
+            {
+                //set the private data 
+                mStockList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                //return the count of the list 
+                return mStockList.Count;
+            }
+            set
+            {
+                //later losers !!!
+            }
+        }
+
+        public ClsStock ThisStock { get; set; }
+
+    }
+}
