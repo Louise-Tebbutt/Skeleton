7e651eef6d538944af791da5796dd8669cc4e5cf
P2794998 <lukeha408@gmail.com>
2025-05-26 23:05:51 +0100

    finished prac 12

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        // if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            // update the list box
+            DisplayStaff();
+        }
+    }
 
+    void DisplayStaff()
+    {
+        // Create an instance of the staff collection
+        clsStaffCollection AllStaff = new clsStaffCollection();
+
+        // Set the data source to list of staff in the collection
+        lstStaffList.DataSource = AllStaff.StaffList;
+
+        // Set the name of the primary key
+        lstStaffList.DataValueField = "StaffId";
+
+        // Set the data field to display
+        lstStaffList.DataTextField = "StaffName";
+
+        // bind the data to the list
+        lstStaffList.DataBind();
     }
 }
--- ClassLibrary/clsStaffCollection.cs
+++ ClassLibrary/clsStaffCollection.cs
@@ -1,9 +1,80 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary
 {
     public class clsStaffCollection
     {
-        public List<clsStaff> StaffList { get; set; }
+        List<clsStaff> mStaffList = new List<clsStaff>();
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                // Return the private data
+                return mStaffList;
+            }
+            set
+            {
+                // Set the private data
+                mStaffList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                // Return the count of the list
+                return StaffList.Count;
+            }
+            set
+            {
+                // later
+            }
+        }
+
+        public clsStaff ThisStaff { get; set; }
+
+        // Constructor for the class
+        public clsStaffCollection()
+        {
+            // Variable for the index
+            Int32 Index = 0;
+
+            // Variable to store the record count
+            Int32 RecordCount = 0;
+
+            // Object for the data connect
+            clsDataConnection DB = new clsDataConnection();
+
+            // Execute the stored procedure
+            DB.Execute("sproc_tblStaff_SelectAll");
+
+            // Get the count of record
+            RecordCount = DB.Count;
+
+            // While there are records to process
+            while (Index < RecordCount)
+            {
+                // Create a blank address
+                clsStaff AnStaff = new clsStaff();
+
+                // Read in the fields for the current records
+                AnStaff.StaffId = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffId"]);
+                AnStaff.StaffName = Convert.ToString(DB.DataTable.Rows[Index]["StaffName"]);
+                AnStaff.StaffEmail = Convert.ToString(DB.DataTable.Rows[Index]["StaffEmail"]);
+                AnStaff.StaffPhoneNumber = Convert.ToString(DB.DataTable.Rows[Index]["StaffPhoneNumber"]);
+                AnStaff.StaffSalary = Convert.ToDecimal(DB.DataTable.Rows[Index]["StaffSalary"]);
+                AnStaff.JoinDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["JoinDate"]);
+                AnStaff.MorePermissions = Convert.ToBoolean(DB.DataTable.Rows[Index]["MorePermissions"]);
+
+                // Add the record to the private data member
+                mStaffList.Add(AnStaff);
+
+                // Point at the next record
+                Index++;
+
+            }
+        }
     }
 }
