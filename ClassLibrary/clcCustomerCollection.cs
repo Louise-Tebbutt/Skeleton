9bb15a892a81633899cfa6b767ebf114623ed11b
p2785371 <p2785371@my365.dmu.ac.uk>
2025-06-05 01:14:11 +0100

    part of practical 12

--- ClassLibrary/clcCustomerCollection.cs
+++ ClassLibrary/clcCustomerCollection.cs
@@ -7,18 +7,35 @@ namespace ClassLibrary
     {
         public clcCustomerCollection() 
         {
-            //create the items of test data
-            clsCustomer TestItem = new clsCustomer();
-            //set its properties
-            TestItem.CustomerNo = 5;
-            TestItem.Email = "AshokaTano@outlook.com";
-            TestItem.DateOfBirth = DateTime.Now;
-            TestItem.FullName = "AshokaTanno";
-            TestItem.Address = "112 Ahsoka Street";
-            TestItem.PhoneNumber = "1234567890";
-            TestItem.Active = true;
-            //add the test item to the test list
-            mCustomerList.Add(TestItem);
+            //variable for the index
+            Int32 Index = 0;
+            //varible to show the record count
+            Int32 RecordCount = 0;
+            //object for the data connect
+            clsDataConnection DB = new clsDataConnection();
+            //execute the sotred procedure
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            //get the count of reconrds
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //creates a blank address
+                clsCustomer AnCustomer = new clsCustomer();
+                //read in the feilds for the current record
+                AnCustomer.CustomerNo = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerNo"]);
+                AnCustomer.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                AnCustomer.DateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOfBirth"]);
+                AnCustomer.FullName = Convert.ToString(DB.DataTable.Rows[Index]["FullName"]);
+                AnCustomer.Address = Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                AnCustomer.PhoneNumber = Convert.ToString(DB.DataTable.Rows[Index]["PhoneNumber"]);
+                AnCustomer.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                //add the record to the private data meber
+                mCustomerList.Add(AnCustomer);
+                //point at the next record
+                Index++;
+            }
+            
 
         }
 
